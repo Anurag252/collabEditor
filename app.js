e73adfb9c5dfc640f950f58b8742fd6e97210fef
Anurag <Anuragsgsits@gmail.com>
2019-01-20 02:36:25 +0530

    removing hardcoded port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(process.env.PORT || 80);
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/test', function (req, res) {
